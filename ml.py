59bbd7580098883d36daaed0053c751cc5f963bd
Halil Ibrahim Celikel <169132075+halilice@users.noreply.github.com>
2024-08-15 16:07:50 +0200

    Update ml.py

--- ml.py
+++ ml.py
@@ -6,6 +6,23 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import NearestNeighbors
 
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("https://i.postimg.cc/4xgNnkfX/Untitled-design.png");
+background-size: cover;
+background-position: center center;
+background-repeat: no-repeat;
+background-attachment: local;
+}}
+[data-testid="stHeader"] {{
+background: rgba(0,0,0,0);
+}}
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
 
 def get_recom():
     # User's input
